66afa86dfe6346a48f16c019b9850488ea404dbe
Rickard Edberg <rickard.edberg@gmail.com>
2024-09-19 11:47:56 +0200

    added some variables and equations

--- Program.cs
+++ Program.cs
@@ -3,5 +3,13 @@
     public static void Main(string[] args)
     {
         Console.WriteLine("Något rimligt");
+
+        //här lägger vi till lite variabler och gör en uträkning.
+        int num1 = 17;
+        int num2 = 36;
+        int num3 = 3;
+        Console.WriteLine(num1+num2*num3);
+        Console.WriteLine(num3/num1+num2);
+        Console.WriteLine(num2-num3+num1);
     }
 }
